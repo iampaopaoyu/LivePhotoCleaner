95b92d6498b97dfb4643737ad65b1c38fedd6179
Marco Schillinger <marco.schillinger@mathema.de>
2021-09-24 14:58:22 +0200

    set default values for settings

--- LivePhotoCleaner/LivePhotoCleanerApp.swift
+++ LivePhotoCleaner/LivePhotoCleanerApp.swift
@@ -21,6 +21,16 @@ struct LivePhotoCleanerApp: App {
 
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
+    init() {
+            // this could be read from a plist file as well, there should be no difference in functionality
+            // as we've got our Keys enum we set the defaults here to make sure we do not have different string values
+        UserDefaults.standard.register(defaults: [
+            Constants.includeIcloudImages: false,
+            Constants.deleteLivePhotos: true,
+            Constants.moveToAlbum: true,
+        ])
+    }
+
     var body: some Scene {
         WindowGroup {
             PhotoOverview()
